b71fd72ec6a84d9dfb29af6afd5630a1f74c841f
AdityaKumbhar07 <kumbharaditya777@gmail.com>
2026-09-16 17:50:11 +0530

    feat(native): scffold cmake and c++ toolchain

---
+++ app/src/main/cpp/shadowvault_core.cpp
@@ -0,0 +1,21 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("svault");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("svault")
+//      }
+//    }
+
+#include <jni.h>
+
+// libshadowvault_core — Native C++20 Cryptographic Core
